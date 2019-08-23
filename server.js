97c8733f4046c3e407dc57555dddb4a46651a740
ivansyvak <ivansyvak1991@gmail.com>
2019-08-23 14:48:58 +0300

    few changes

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
 app.use('/static', express.static(__dirname + '/static'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
@@ -12,7 +18,8 @@ app.get('/', function (req, res) {
 });
 
 app.post('/location', (req, res) => {
-  http.get('http://93.79.103.147:3000/location?'+req.body);
+  http.get('http://93.79.103.147:3000/location?data='+JSON.stringify(req.body));
+  res.end();
 });
 
 app.get('/location', (req, res) => {
